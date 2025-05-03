8429a56006ba05df82aae73f1ab0de27c3cef42e
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-05-04 02:37:14 +0300

    created fn start #04.05.2025

--- logic.js
+++ logic.js
@@ -1,40 +1,89 @@
 let players = ["x", "o"];
 let activePlayer = 0;
-
 let playingField = [];
+let winningСombinations = [];
 
 
-function checkNumberCellsPerRow(value) {
+function checkNumberCellsPerRow(numberCells) {
 
-  if (isNaN(value)) {
+  if (isNaN(numberCells)) {
     return 3;
   }
 
-  return +value < 3 ? 3 : +value;
+  numberCells = +numberCells < 3 ? 3 : +numberCells;
+
+  return numberCells;
 }
 
+function renderWinningСombinations(numberCells) {
+
+    // Формируем выигрышные комбинации по строкам и столбцам
+    let storage = [];
+
+    let flatPlayingField = playingField.flat();
+
+    flatPlayingField.flat().forEach((item, index) => {
+      storage.push(index);
+
+      if (storage.length === numberCells) {
+        // winningСombinations.push(storage.join());
+        winningСombinations.push(storage);
+        storage = [];
+      }
+    });
+
+  // Формируем выигрышную комбинацию по диагонали с лева на право
+    for (let index = 0; index < flatPlayingField.length; index += numberCells + 1) {
+      storage.push(index);
+    }
+
+    winningСombinations.push(storage);
+    storage = [];
+
+    // Формируем выигрышную комбинацию по диагонали с право yf ktdj
+    for (let index = numberCells - 1; index < flatPlayingField.length - 1 ; index += numberCells-1) {
+      storage.push(index);
+    }
+
+    winningСombinations.push(storage);
+    storage = [];
+
+}
+
+
 function startGame() {
+
+  // Перед каждой игрой очищаем поле
+  playingField = [];
+
   let numberCells;
-  // numberCells = prompt("Введите кол-во ячеек в строке:");
+  // Это задел, если дать пользователю выбирать кол-во ячеек для игры
+  // numberCells = prompt("Введите числом кол-во ячеек от 3 и более:");
 
   let numberCellsPerRow = checkNumberCellsPerRow(numberCells);
 
   let line = Array(numberCellsPerRow).fill('');
 
-  // Для ускорения цикла используется модуль из C++
+  // Для ускорения цикла используется модуль из языка C++
   for (let c = 0; c < numberCellsPerRow; c++) {
     playingField.push(line.slice());
   }
 
   renderBoard(playingField);
 
+  renderWinningСombinations(numberCellsPerRow);
+  console.log(winningСombinations);
+
   players[activePlayer];
 }
 
 function click(line, column) {
+
   playingField[line][column] = players[activePlayer];
-  console.log(playingField);
+
   renderBoard(playingField);
+
+  // showWinner(activePlayer);
 
 
   activePlayer = activePlayer === 0 ? 1 : 0;
